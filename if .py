df37409975c221fd066cc348b2fc2b8b7bab9268
hafsa12abdullah-sketch <hafsa12abdullah@gmail.com>
2026-07-23 11:01:42 +0500

    Add files via upload

---
+++ if .py
@@ -0,0 +1,172 @@
+age = 20
+
+if age >= 18:
+    print("Adult")
+    
+    
+age = 30
+
+if age >= 10:
+    print("young") 
+    
+else:
+    print("adult")  
+    
+    
+age =20
+
+if age <=15:
+    print("young")
+    
+else:
+    print("elder")        
+    
+    
+score = 80
+
+if score >= 90:
+    print("grade A")
+elif score >= 60:
+    print("grade B")
+elif score >= 50:
+    print("grade C")
+else:
+    print("fail")
+    
+
+# Check karna ke kya number positive hai aur sath hi even (koft) hai?
+num = 10
+
+if num > 0:
+    print("Number positive hai.")
+    if num % 2 == 0:
+        print("Aur yeh even number bhi hai.")
+    else:
+        print("Lekin yeh odd number hai.")
+
+
+
+score = 85
+
+if score >= 90:
+    print("A+")
+elif score >= 80:
+    print("A")
+else:
+    print("Pass")
+    
+    
+
+students = 40
+
+if students <=30:
+    print("10 students")
+elif students >=10:        
+    print("5 students")
+else:
+    print("15 students")    
+    
+    
+    
+# Sirf special status check karne ke liye
+role = "admin"
+
+if role == "admin":
+    print("Full Access")
+elif role == "editor":
+    print("Edit Access")    
+    
+    
+    
+role = input("managment: ")
+
+if role == "admin":
+    print("Full Access")
+elif role == "editor":
+    print("Edit Access")
+else:
+    print("Access Denied (managment)")    
+
+
+
+
+data = 60
+if data>=20:
+    print("data:big")
+elif data>=30:
+    print("data:small")
+else:
+    print("data:large")                    
+    
+    
+    
+age = 30
+
+if age <=20:
+    print("you are young")
+    
+else:
+    print("you are elder")     
+    
+    
+    
+students = 40
+
+if students <=25:
+    print("High Strength")
+ 
+else:
+    print("Low Strength") 
+    
+    
+    
+students = 30
+
+if students >=25:
+    print("High Strength")
+    
+elif students <=10:
+    print("Middle Strength")    
+ 
+else:
+    print("Low Strength") 
+    
+    
+    
+temperature = 30
+
+if temperature > 25:
+    print("It's a hot day!")
+    
+else:
+    print ("It's a cold day!")   
+    
+    
+    
+marks = 60
+
+if marks >=20:
+    print("lower marks")
+    
+elif marks <=40:
+    print("maximum marks")
+    
+elif marks >=50:
+    print("high marks")
+ 
+else:
+    print("good marks")                     
+    
+    
+    
+students = 15
+
+if students <=10:
+    print("low strength")
+    
+else:
+    print("high strength")    
+        
+
+    
+    
