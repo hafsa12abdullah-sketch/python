70ad7b2688d5168a70582a36321203e7890c3156
hafsa12abdullah-sketch <hafsa12abdullah@gmail.com>
2026-07-23 11:00:26 +0500

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,38 @@
+for h in range(6):
+    print(h)
+    
+data= {
+    "age":[22,50,34,15,70],
+    "city":["karachi','lahore","korangi","panjab","islamabad"],
+    "marks":[80,50,40,90,30],
+    "name":["maha","neha","isra","iqra","warda"],
+    "id number":[101,102,103,104,105]
+}    
+for value in data.values():
+    print(value)
+    
+    
+data= {
+    "age":[22,50,34,15,70],
+    "city":["karachi','lahore","korangi","panjab","islamabad"],
+    "marks":[80,50,40,90,30],
+    "name":["maha","neha","isra","iqra","warda"],
+    "id number":[101,102,103,104,105]
+}    
+for key in data.keys():
+    print(key)    
+    
+    
+data= {
+    "age":[22,50,34,15,70],
+    "city":["karachi','lahore","korangi","panjab","islamabad"],
+    "marks":[80,50,40,90,30],
+    "name":["maha","neha","isra","iqra","warda"],
+    "id number":[101,102,103,104,105]
+}    
+for key, value in data.items():
+    print(key,value)       
+    
+    
+    
+    
---
+++ while loop .py
@@ -0,0 +1,50 @@
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+    
+    
+h = 1
+
+while h <= 6:
+    print(h)
+    h += 1  
+    
+
+
+while True:
+    print(i)
+    if i == 5:
+        break
+    i += 1
+    
+    
+i = 0
+
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+    
+    
+    
+    
+i = 1
+
+while i <= 3:
+    print(i)
+    i += 1
+    
+else:
+    print("Loop Completed")     
+    
+    
+    
+i = 1
+
+while i <=6:
+    print(i)
+    i +=1
+             
