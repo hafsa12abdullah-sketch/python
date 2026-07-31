e1beed773d02b198ad407d6c8d37caf567ec83ee
hafsa12abdullah-sketch <hafsa12abdullah@gmail.com>
2026-07-31 10:36:07 +0500

    Add files via upload

---
+++ Python_calculator.py
@@ -0,0 +1,22 @@
+num1=int(input("enter your first number:"))
+num2=int(input("enter your second number:"))
+
+#ADDITION
+
+add = num1+num2
+print("add=",add)
+
+#SUBSTRACTION
+
+sub = num1-num2
+print("sub=",sub)
+
+#DIVISION
+
+div = num1/num2
+print("div=",div)
+
+#MULTIPLICATION
+
+mul = num1*num2
+print("mul=",mul)
